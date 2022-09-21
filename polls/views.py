8ae6c7219a7ee866e4dc34e90e63b0af08970419
Ermek Bakytaev <107106835+bakytaev@users.noreply.github.com>
2022-09-21 16:05:41 +0600

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse(f"Вы находитесь на вопросе {str(question_id)}")
+    
+
+def result(result, question_id):
+    return HttpResponse(f"Вы находитесь на результатах вопросе {str(question_id)}")
+    
+
+def vote(result, question_id):
+    return HttpResponse(f"Вы проголосовали за вопрос №{str(question_id)}")
